537940d6176e326b5d31cbb6fb5381d0d075ae05
leomuko <kezzieleo@gmail.com>
2022-03-11 16:58:53 +0300

    Create App Utility Files

---
+++ app/src/main/java/com/example/scaliointerview/utilities/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.scaliointerview.utilities
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://api.github.com/"
+    }
+}
---
+++ app/src/main/java/com/example/scaliointerview/utilities/InternetDetection.kt
@@ -0,0 +1,19 @@
+package com.example.scaliointerview.utilities
+
+import android.content.Context
+import android.net.ConnectivityManager
+
+object InternetDetection {
+    fun isInternetAvailable(context : Context) : Boolean{
+        val info = (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).activeNetworkInfo
+        return if (info == null){
+            false
+        }else{
+            if (info.isConnected){
+                true
+            }else{
+                true
+            }
+        }
+    }
+}
